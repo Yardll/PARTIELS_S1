c427d11352a2e536c597d7594763a2c61ca8811c
Yanis Belkadi <49492560-yayanisse@users.noreply.replit.com>
2026-01-16 09:57:27 +0000

    Premier commit

---
+++ PARTIELS_S1/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  const float coutHoraireMeca = 4.0;
+  const float coutHoraireElec = 5.0;
+  int velosDispos = 20;
+  int capacite= 20;
+  int choix;
+
+  cout <<"****** Gestion d'une station de Velo ******\n";
+  cout <<"--------------------------------------------\n";
+
+
+
+  cout<<"---------------------------------------------\n";
+  cout<<"1. Emprunter un velo\n2. Restituer un velo\n3. Estimer le cout de la location\n4.Quitter l'application\n";
+
+  return 0;
+}
